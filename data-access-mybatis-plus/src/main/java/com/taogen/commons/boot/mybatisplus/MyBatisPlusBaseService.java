bd4e600b2f16b6bf780831e58a4c8a68ab76960c
taogen <taogenjia@gmail.com>
2024-03-07 19:54:21 +0800

    ✨feat: Initial commit MyBatisPlusBaseService

---
+++ data-access-mybatis-plus/src/main/java/com/taogen/commons/boot/mybatisplus/MyBatisPlusBaseService.java
@@ -0,0 +1,13 @@
+package com.taogen.commons.boot.mybatisplus;
+
+/**
+ * MyBatis Plus Base Service
+ * <p>
+ * Implement related data query and update. And doesn't use reflection.
+ *
+ * @author taogen
+ */
+public abstract class MyBatisPlusBaseService {
+    // TODO
+}
+
